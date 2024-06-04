f009c26add6e0e88242bb9ffdedcd8a25b333af5
Othman AlJundi <59793292+OthmanAljundi-01@users.noreply.github.com>
2024-06-04 11:02:35 +0300

    Update manifest-create.sh

--- manifest-create.sh
+++ manifest-create.sh
@@ -1,14 +1,1 @@
-cat > manifest.json<< EOF
-[{
-  "name": "upn",
-  "source": null,
-  "essential": false,
-  "additionalProperties": []
-},
-{
-"name": "email",
-  "source": null,
-  "essential": false,
-  "additionalProperties": []
-}]
-EOF
+

598e2c2eafff1260bd9309cf7c47b6559bd9c24e
Othman AlJundi <59793292+OthmanAljundi-01@users.noreply.github.com>
2024-06-04 11:01:41 +0300

    Update setup-oc-tool.bash
    empty the content of this file

--- setup-oc-tool.bash
+++ setup-oc-tool.bash
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-echo you can also refer to the following link for the latest release of OC : https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/
-
-mkdir -p /opt/openshift/bin
-mkdir -p /opt/openshift/source
-cd /opt/openshift/source
-wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
-
-tar -zxvf openshift-client-linux.tar.gz -C /opt/openshift/bin
-echo 'export PATH=$PATH:/opt/openshift/bin' >> ~/.bashrc && source ~/.bashrc
-echo 'export PATH=$PATH:/opt/openshift/bin' >> ~/.bash_profile
-
-/opt/openshift/bin/oc completion bash > oc_bash_completion
-sudo cp oc_bash_completion /etc/bash_completion.d/
 
